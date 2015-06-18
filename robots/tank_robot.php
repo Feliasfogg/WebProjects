1da6ec37866e063a4b27b2bd1a19ec23b3559210
Pavel Chehov <good-1991@mail.ru>
2015-06-18 17:56:20 +0300

    robots update

--- robots/tank_robot.php
+++ robots/tank_robot.php
@@ -24,24 +24,35 @@ $last_num=false;
 $vk = new vk( $token, $delta, $app_id, $group_id );
 
 writeLog("start", $log_file);
-$comments=$vk->getComments($post_id, 2,"desc");
-for($i=1; $i<sizeof($comments)-1;++$i){
-	(int) $comment1=(int) $comments[$i]->text;
-	(int) $comment2=(int) $comments[$i+1]->text;
-	if($comment1 && $comment2) {
-		if(($comment2-$comment1)!=1){
-			$comment2>$comment1?$last_num=$comment2:$last_num=$comment1;
-			for($j=0; $j < 2; ++$j){
-				$user=$vk->getOneUser($comments[$i+$j]->from_id);
-				$date = date("H:i", $comments[$i+$j]->date);
-				writeLog("$user->first_name $user->last_name id$user->uid date: $date "."text: ".$comments[$i+$j]->text, $users_log);
+for($k=2; $k<10; ++$k){
+	$comments=$vk->getComments($post_id, $k,"desc");
+	for($i=1; $i<sizeof($comments)-1;++$i){
+		(int) $comment1=(int) $comments[$i]->text;
+		(int) $comment2=(int) $comments[$i+1]->text;
+		if($comment1 && $comment2) {//блок запустится, если оба комментария являются числами
+			if(($comment2-$comment1)!=1){//если два числа отличаются более чем на единицу, выбираем большее из них
+				$comment2>$comment1?$last_num=$comment2:$last_num=$comment1;
+				for($j=0; $j < 2; ++$j){//записываем обоих пользователей в лог
+					$user=$vk->getOneUser($comments[$i+$j]->from_id);
+					$date = date("H:i", $comments[$i+$j]->date);
+					writeLog("$user->first_name $user->last_name id$user->uid date: $date "."text: ".$comments[$i+$j]->text, $users_log);
+				}
+			} else { //Если отлчиче в 1  то выбираем последний из них
+				$last_num=$comment1;
+				break;
 			}
+		} else{
+			//определяем какой именно комментарий является числом, либо у нас обе строки
+			if($comment1 !=0 ) $last_num = $comment1;
+			if($comment2 !=0 ) $last_num = $comment2;
+			if($last_num) break;//если нашли число - выходим из внутреннего цикла
 		}
 	}
+	if($last_num) break;//если нашли число - выходим из внешнего цикла
+	//иначе - берем массив из уже 3,4,5,6 комментариев до тех пор пока не дойдем до числа
 }
-if(!$last_num) (int) $last_num = (int) $comments[1]->text;
 
-if(rand(0, 99) < 5 && $comments[1]->from_id != "152223765" && $last_num > 0 && $last_num < 25000) {
+if(rand(0, 99) < 10 && $comments[1]->from_id != "152223765" && $last_num > 0 && $last_num < 25000) {
 	if($vk->setOnline()) writeLog("set online", $log_file);
 	else writeLog("cant set online", $log_file);
 	$count=rand(1,2);//определяет вероятность запостить 2 или 1 пост
