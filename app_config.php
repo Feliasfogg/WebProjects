d10e0fc8e53eb3b95ebcba618591350eec3a3739
feliasfogg <good-1991@mail.ru>
2016-03-28 00:24:18 +0300

    Add resumeupdater for hh.ru

--- app_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_name    = "user1137761_testn";
-$dbhost     = "localhost"; // Имя хоста БД
-$dbusername = "fatatwork"; // Пользователь БД
-$dbpass     = "nyantech22"; // Пароль к базе
-$token="c76ae506cd10e151a03b47f0ca8160552cb64ac8799bfddeecebd44674b4175ff25323df2933c530470fb&expires_in=0&user_id=152223765";
-?>
